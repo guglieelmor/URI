1d8fb91833ada575b476886857b4f06c7c66e50d
guglieelmor <guglieelmor@gmail.com>
2020-03-10 17:28:34 -0300

    Códigos

---
+++ iniciante.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<math.h>
+
+
+ main() {
+	
+	int cod1, cod2, num1, num2, valor1, valor2;
+	
+	scanf
+
+	
+	/*	//1009
+	char nome[60];
+	double salario, vendas, total;
+	
+	scanf("%s %lf %lf", &nome, &salario, &vendas);
+
+	total=salario+vendas*0.15;
+	
+	printf("TOTAL = R$ %.2lf\n", total);
+
+		
+	//1008
+	int func;
+	float total, horas, valorHora;
+	
+	scanf("%i %f %f", &func, &horas, &valorHora);
+	
+	total = valorHora*horas;
+	
+	printf("NUMBER = %i\n", func);
+	printf("SALARY = U$ %.2f\n", total);	
+	
+	
+	//1007
+	int A, B, C, D, DIFERENCA;
+	
+	scanf("%i %i %i %i", &A, &B, &C, &D);
+	
+	DIFERENCA = (A*B-C*D);
+	
+	printf("DIFERENCA = %i\n", DIFERENCA);
+	
+	
+	
+	//1006
+	double A, B, C, MEDIA;
+	
+	scanf("%lf %lf %lf", &A, &B, &C);
+	
+	MEDIA = ((A*2)+(B*3)+(C*5))/10;
+	
+	printf("MEDIA = %.1lf\n", MEDIA);
+	
+	
+	//1005
+	double A, B, MEDIA;
+	
+	scanf("%lf %lf", &A, &B);
+	
+	MEDIA = ((A*3.5)+(B*7.5))/11;
+	
+	printf("MEDIA = %.5lf\n", MEDIA);
+	
+	
+	//1004
+	int A, B, PROD;
+	
+	scanf("%i %i", &A, &B);
+	
+	PROD = A * B;
+	
+	printf("PROD = %i\n", PROD);
+	
+	//1003	
+	int SOMA, A, B;
+	
+	scanf("%i %i", &A, &B);
+	
+	SOMA = A + B;
+	
+	printf("SOMA = %i\n", SOMA);
+ 
+    return 0;
+    
+    //1002
+    double raio, n, area;
+	
+	n = 3.14159;
+	
+	scanf("%lf", &raio);
+	
+	area=n*(pow(raio,2));
+	
+	printf("A=%.4f\n", area);
+	
+    //1001
+    int X=0,A=0, B=0;
+	scanf("%i", &A);
+	scanf("%i", &B);
+	
+	X=A+B;
+	
+	printf("X = %i", X);*/
+	
+	return 0;
+}
+	
+
+
